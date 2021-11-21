f98df9cc1c44a371396bbead870abb31e11619a8
dikac <rigel234@yahoo.com>
2021-11-21 20:39:42 +0700

    recompile

--- dist/validator.js
+++ dist/validator.js
@@ -1,14 +1,2 @@
 export {};
-//
-// type Validator<
-//     Allow = any,
-//     Expectation extends Allow = Allow,
-//     Allowed extends boolean = boolean,
-//     Expected extends boolean = boolean,
-//     Validatable extends ValidatableType<Allow> = ValidatableType<Allow>
-//     > =
-//     <AllowArgument extends Allow, ExpectedArgument extends Expectation>(value : AllowArgument|ExpectedArgument) =>
-//         Static<Allow, AllowArgument, ExpectedArgument, Allowed, Expected, Validatable>
-//
-// export default Validator;
 //# sourceMappingURL=validator.js.map
